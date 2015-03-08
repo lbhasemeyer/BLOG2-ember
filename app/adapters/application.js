72aa1b277350b5cfb714b50fa6beb5921a4ac4cd
lkbhas <lbhasemeyer@gmail.com>
2015-03-08 17:19:23 -0600

    heroku adjustments

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from "ember-data";
-import ENV from "../config/environment";
+import ENV from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-  host: ENV.adapterURL || ENV.ADAPTER_URL
+    host: ENV.adapterURL || ENV.ADAPTER_URL
 });
--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     baseURL: '/',
     locationType: 'auto',
     adapterURL: process.env.ADAPTER_URL,
+    adapterURL: process.env.ADAPTER_URL,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
