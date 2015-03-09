3671ad0f6689bf3274a562edd272f3df38a8e87b
lkbhas <lbhasemeyer@gmail.com>
2015-03-09 13:41:33 -0600

    heroku push

--- config/environment.js
+++ config/environment.js
@@ -5,6 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'ember-front',
     environment: environment,
     baseURL: '/',
+    adapterURL: process.env.ADAPTER_URL,
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
